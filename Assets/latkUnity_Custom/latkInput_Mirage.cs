cc34d14ac78ca783496bf142aa95d32312db675d
Nick Fox-Gieg <nick@fox-gieg.com>
2019-01-07 18:48:28 -0600

    test drawing works

---
+++ Assets/latkUnity_Custom/latkInput_Mirage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class latkInput_Mirage : MonoBehaviour {
+
+    public LightningArtist latk;
+    public GvrTrackedController ctl0;
+    public GvrTrackedController ctl1;
+
+    private void Awake() {
+        if (latk == null) latk = GetComponent<LightningArtist>();
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if (ctl0.ControllerInputDevice.GetButton(GvrControllerButton.Trigger) || ctl1.ControllerInputDevice.GetButton(GvrControllerButton.Trigger)) { 
+            latk.clicked = true;
+        } else {
+            latk.clicked = false;
+        }
+    }
+
+}
