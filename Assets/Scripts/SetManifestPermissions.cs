63487166ec8dc08c37eb8b2d330fb006083d29ed
Nick Fox-Gieg <nick@fox-gieg.com>
2020-01-14 16:37:55 -0500

    Tweak ui, permission testing

--- Assets/Scripts/SetManifestPermissions.cs
+++ Assets/Scripts/SetManifestPermissions.cs
@@ -1,42 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gvr.Internal;
 
 // GVR apps don't use the standard Android manifest method of setting permissions.
 
-public class SetManifestPermissions : MonoBehaviour {
+namespace GoogleVR.PermissionsDemo {
 
-    private string[] permissionNames = {
-		"READ_EXTERNAL_STORAGE",
-		"CAMERA",
-		"RECORD_AUDIO",
-		"READ_EXTERNAL_STORAGE",
-		"WRITE_EXTERNAL_STORAGE",
-		"READ_INTERNAL_STORAGE",
-		"WRITE_INTERNAL_STORAGE",
-		"INTERNET",
-		"ACCESS_NETWORK_STATE",
-		"GET_ACCOUNTS"
+    public class SetManifestPermissions : MonoBehaviour {
+
+        private string[] permissionNames = {
+        "READ_EXTERNAL_STORAGE",
+        "CAMERA",
+        "RECORD_AUDIO",
+        "READ_EXTERNAL_STORAGE",
+        "WRITE_EXTERNAL_STORAGE",
+        "READ_INTERNAL_STORAGE",
+        "WRITE_INTERNAL_STORAGE",
+        "INTERNET",
+        "ACCESS_NETWORK_STATE",
+        "GET_ACCOUNTS"
     };
 
-    private List<GvrPermissionsRequester.PermissionStatus> permissionList = new List<GvrPermissionsRequester.PermissionStatus>();
+        private List<GvrPermissionsRequester.PermissionStatus> permissionList = new List<GvrPermissionsRequester.PermissionStatus>();
 
-    private void Awake() {
-		for (int i=0; i<permissionNames.Length; i++) {
-            permissionNames[i] = "android.permission." + permissionNames[i];
-		}
-        GvrPermissionsRequester permissionRequester = GvrPermissionsRequester.Instance;
-        permissionRequester.RequestPermissions(permissionNames,
-            (GvrPermissionsRequester.PermissionStatus[] permissionResults) => {
-                permissionList.Clear();
-                permissionList.AddRange(permissionResults);
-                string msg = "";
-                foreach (GvrPermissionsRequester.PermissionStatus p in permissionList) {
-                    msg += p.Name + ": " + (p.Granted ? "Granted" : "Denied") + "\n";
-                }
+        private void Start() {
+            for (int i = 0; i < permissionNames.Length; i++) {
+                permissionNames[i] = "android.permission." + permissionNames[i];
+            }
+            GvrPermissionsRequester permissionRequester = GvrPermissionsRequester.Instance;
+            permissionRequester.RequestPermissions(permissionNames,
+                (GvrPermissionsRequester.PermissionStatus[] permissionResults) => {
+                    permissionList.Clear();
+                    permissionList.AddRange(permissionResults);
+                    string msg = "";
+                    foreach (GvrPermissionsRequester.PermissionStatus p in permissionList) {
+                        msg += p.Name + ": " + (p.Granted ? "Granted" : "Denied") + "\n";
+                    }
 
-                Debug.Log(msg);
-            });
+                    Debug.Log(msg);
+                });
+        }
+
     }
 
 }
